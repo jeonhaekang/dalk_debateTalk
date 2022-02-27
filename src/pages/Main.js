640131b6534f46a643ebc1a18a31ac3566afbaf9
jeonhaekang <73621658+jeonhaekang@users.noreply.github.com>
2022-02-27 21:45:32 +0900

    메인 그리드 끝

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,115 +1,65 @@
 import React from "react";
 import Header from "../shared/Header";
 import Grid from "../elements/Grid";
+import MainCarousel from "../components/main/MainCarousel";
+import TopRank from "../components/main/TopRank";
+import MainCard from "../components/main/MainCard";
+import MainCategoryCard from "../components/main/MainCategoryCard";
+import styled from "styled-components";
 
 const Main = (props) => {
+  const userRankList = ["카리스마 대빵 큰 오리", "카리스마 오리", "큰 오리"];
+
   return (
     <>
-      <Grid height="100%" overflow="scroll" display="block">
+      <Grid height="100%" overflow="scroll">
         <Header page="메인" />
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi rhoncus
-        magna mi, in lobortis urna molestie ut. Quisque faucibus sed est nec
-        efficitur. Aenean nisi dui, lacinia non elementum id, eleifend a mauris.
-        Nulla facilisi. Etiam eleifend tellus mauris, ut bibendum mi ornare id.
-        Duis eu pellentesque quam. Ut efficitur ex eget velit porta porta.
-        Vestibulum sed ex lacinia, malesuada lectus venenatis, consequat mauris.
-        Sed viverra, sapien sit amet molestie laoreet, augue ipsum pharetra
-        orci, ac vulputate lorem lacus nec massa. Donec tellus ligula, viverra
-        id viverra nec, eleifend vitae sem. Nam tincidunt magna a quam
-        condimentum mollis. Praesent eget porttitor orci. Donec semper vitae sem
-        at viverra. Sed quis fringilla est. Class aptent taciti sociosqu ad
-        litora torquent per conubia nostra, per inceptos himenaeos. Nullam ac
-        sodales lectus. Nulla ultrices id risus sed ullamcorper. In laoreet
-        augue neque. Sed dui nisl, cursus nec nunc eu, luctus luctus purus. Sed
-        dapibus nec nisl in mollis. Integer quam dolor, vulputate volutpat
-        libero eget, rutrum ullamcorper nulla. Mauris eu placerat purus, a
-        rutrum mauris. Ut bibendum vel lacus ac molestie. Nam vehicula libero
-        magna, eu condimentum sem volutpat et. Donec ultricies at tellus ac
-        eleifend. Ut orci risus, egestas eu sem non, pretium condimentum justo.
-        Morbi tempus orci eget lorem dignissim finibus. Quisque tincidunt non
-        justo nec fermentum. Ut mattis arcu a nisi lacinia hendrerit. Ut
-        sagittis porta nunc nec faucibus. Aenean ut mattis sem. Pellentesque at
-        mollis odio. Nulla quis pretium erat, ac porta magna. Maecenas sit amet
-        condimentum dolor. Aliquam a leo erat. Ut interdum condimentum egestas.
-        Curabitur mollis tempor erat in scelerisque. Cras vel ipsum quam.
-        Maecenas imperdiet nunc at odio pretium, id sodales orci facilisis. Nam
-        vel massa augue. Lorem ipsum dolor sit amet, consectetur adipiscing
-        elit. Vestibulum eu est gravida, semper massa nec, porttitor mauris.
-        Aenean commodo quam ac urna sollicitudin volutpat. Nulla ullamcorper sed
-        magna eu ornare. Sed eget lacus sem. Pellentesque lobortis dolor quam,
-        ornare volutpat orci congue eget. Donec luctus maximus risus, in
-        pharetra enim hendrerit a. In et molestie tellus. Sed sagittis nulla
-        tellus, sed ullamcorper dolor scelerisque eget. Lorem ipsum dolor sit
-        amet, consectetur adipiscing elit. Morbi rhoncus magna mi, in lobortis
-        urna molestie ut. Quisque faucibus sed est nec efficitur. Aenean nisi
-        dui, lacinia non elementum id, eleifend a mauris. Nulla facilisi. Etiam
-        eleifend tellus mauris, ut bibendum mi ornare id. Duis eu pellentesque
-        quam. Ut efficitur ex eget velit porta porta. Vestibulum sed ex lacinia,
-        malesuada lectus venenatis, consequat mauris. Sed viverra, sapien sit
-        amet molestie laoreet, augue ipsum pharetra orci, ac vulputate lorem
-        lacus nec massa. Donec tellus ligula, viverra id viverra nec, eleifend
-        vitae sem. Nam tincidunt magna a quam condimentum mollis. Praesent eget
-        porttitor orci. Donec semper vitae sem at viverra. Sed quis fringilla
-        est. Class aptent taciti sociosqu ad litora torquent per conubia nostra,
-        per inceptos himenaeos. Nullam ac sodales lectus. Nulla ultrices id
-        risus sed ullamcorper. In laoreet augue neque. Sed dui nisl, cursus nec
-        nunc eu, luctus luctus purus. Sed dapibus nec nisl in mollis. Integer
-        quam dolor, vulputate volutpat libero eget, rutrum ullamcorper nulla.
-        Mauris eu placerat purus, a rutrum mauris. Ut bibendum vel lacus ac
-        molestie. Nam vehicula libero magna, eu condimentum sem volutpat et.
-        Donec ultricies at tellus ac eleifend. Ut orci risus, egestas eu sem
-        non, pretium condimentum justo. Morbi tempus orci eget lorem dignissim
-        finibus. Quisque tincidunt non justo nec fermentum. Ut mattis arcu a
-        nisi lacinia hendrerit. Ut sagittis porta nunc nec faucibus. Aenean ut
-        mattis sem. Pellentesque at mollis odio. Nulla quis pretium erat, ac
-        porta magna. Maecenas sit amet condimentum dolor. Aliquam a leo erat. Ut
-        interdum condimentum egestas. Curabitur mollis tempor erat in
-        scelerisque. Cras vel ipsum quam. Maecenas imperdiet nunc at odio
-        pretium, id sodales orci facilisis. Nam vel massa augue. Lorem ipsum
-        dolor sit amet, consectetur adipiscing elit. Vestibulum eu est gravida,
-        semper massa nec, porttitor mauris. Aenean commodo quam ac urna
-        sollicitudin volutpat. Nulla ullamcorper sed magna eu ornare. Sed eget
-        lacus sem. Pellentesque lobortis dolor quam, ornare volutpat orci congue
-        eget. Donec luctus maximus risus, in pharetra enim hendrerit a. In et
-        molestie tellus. Sed sagittis nulla tellus, sed ullamcorper dolor
-        scelerisque eget.Lorem ipsum dolor sit amet, consectetur adipiscing
-        elit. Morbi rhoncus magna mi, in lobortis urna molestie ut. Quisque
-        faucibus sed est nec efficitur. Aenean nisi dui, lacinia non elementum
-        id, eleifend a mauris. Nulla facilisi. Etiam eleifend tellus mauris, ut
-        bibendum mi ornare id. Duis eu pellentesque quam. Ut efficitur ex eget
-        velit porta porta. Vestibulum sed ex lacinia, malesuada lectus
-        venenatis, consequat mauris. Sed viverra, sapien sit amet molestie
-        laoreet, augue ipsum pharetra orci, ac vulputate lorem lacus nec massa.
-        Donec tellus ligula, viverra id viverra nec, eleifend vitae sem. Nam
-        tincidunt magna a quam condimentum mollis. Praesent eget porttitor orci.
-        Donec semper vitae sem at viverra. Sed quis fringilla est. Class aptent
-        taciti sociosqu ad litora torquent per conubia nostra, per inceptos
-        himenaeos. Nullam ac sodales lectus. Nulla ultrices id risus sed
-        ullamcorper. In laoreet augue neque. Sed dui nisl, cursus nec nunc eu,
-        luctus luctus purus. Sed dapibus nec nisl in mollis. Integer quam dolor,
-        vulputate volutpat libero eget, rutrum ullamcorper nulla. Mauris eu
-        placerat purus, a rutrum mauris. Ut bibendum vel lacus ac molestie. Nam
-        vehicula libero magna, eu condimentum sem volutpat et. Donec ultricies
-        at tellus ac eleifend. Ut orci risus, egestas eu sem non, pretium
-        condimentum justo. Morbi tempus orci eget lorem dignissim finibus.
-        Quisque tincidunt non justo nec fermentum. Ut mattis arcu a nisi lacinia
-        hendrerit. Ut sagittis porta nunc nec faucibus. Aenean ut mattis sem.
-        Pellentesque at mollis odio. Nulla quis pretium erat, ac porta magna.
-        Maecenas sit amet condimentum dolor. Aliquam a leo erat. Ut interdum
-        condimentum egestas. Curabitur mollis tempor erat in scelerisque. Cras
-        vel ipsum quam. Maecenas imperdiet nunc at odio pretium, id sodales orci
-        facilisis. Nam vel massa augue. Lorem ipsum dolor sit amet, consectetur
-        adipiscing elit. Vestibulum eu est gravida, semper massa nec, porttitor
-        mauris. Aenean commodo quam ac urna sollicitudin volutpat. Nulla
-        ullamcorper sed magna eu ornare. Sed eget lacus sem. Pellentesque
-        lobortis dolor quam, ornare volutpat orci congue eget. Donec luctus
-        maximus risus, in pharetra enim hendrerit a. In et molestie tellus. Sed
-        sagittis nulla tellus, sed ullamcorper dolor scelerisque eget.
+        <Grid
+          display="flex"
+          flexDirection="column"
+          justifyContent="space-between"
+          gap="10px"
+        >
+          <MainCarousel />
+          <TopRank userRankList={userRankList} />
+          <Grid
+            display="flex"
+            flexDirection="column"
+            justifyContent="space-between"
+            gap="10px"
+          >
+            <MainCard />
+            <MainCard />
+            <MainCard />
+            <MainCard />
+            <Grid>이런 주제로도 토론해요!!</Grid>
+            <CategoryBox>
+              <MainCategoryCard />
+              <MainCategoryCard />
+              <MainCategoryCard />
+              <MainCategoryCard />
+              <MainCategoryCard />
+              <MainCategoryCard />
+              <MainCategoryCard />
+            </CategoryBox>
+            <MainCard />
+            <MainCard />
+            <MainCard />
+          </Grid>
+          <Grid>토론 더보기</Grid>
+        </Grid>
       </Grid>
     </>
   );
 };
 
 Main.defaultProps = {};
+
+const CategoryBox = styled.div`
+  display: flex;
+  flex-wrap: nowrap; // 넘쳐도 줄바꿈 X, white-space: no-wrap과 같은 효과
+  overflow-x: scroll; // x축 넘치면 스크롤 생성
+  gap: 10px;
+`;
 
 export default Main;
