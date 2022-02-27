e130674c0e03a2373521ec05b226c5ae89af6618
jeonhaekang <73621658+jeonhaekang@users.noreply.github.com>
2022-02-27 21:44:26 +0900

    랭킹 애니메이션 구현 완료

---
+++ src/components/main/TopRank.js
@@ -0,0 +1,57 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+import Grid from "../../elements/Grid";
+
+const TopRank = (props) => {
+  const { userRankList } = props;
+  const [userInfo, setUserInfo] = React.useState(`1위 ${userRankList[0]}`);
+
+  React.useEffect(() => {
+    let count = 1;
+
+    // setInterval : 일정한 간격으로 콜백함수 호출
+    const test = setInterval(() => {
+      setUserInfo(`${count + 1}위 ${userRankList[count]}`);
+      if (userRankList.length - 1 > count) count++;
+      else count = 0;
+    }, 3000);
+
+    return () => {
+      clearInterval(test);
+    };
+  }, []);
+
+  return (
+    <Grid
+      height="40px"
+      backgroundColor="gray"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Rank key={userInfo}>{userInfo}</Rank>
+    </Grid>
+  );
+};
+
+const fadeIn = keyframes`
+from {
+  opacity: 0;
+  transform: translateY(20px);
+}
+to {
+  opacity: 3;
+  transform: none;
+}
+`;
+
+const Rank = styled.div`
+  animation: ${fadeIn} 0.4s;
+`;
+
+TopRank.defaultProps = {
+  userRankList: [],
+};
+
+export default TopRank;
