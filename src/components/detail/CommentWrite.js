52a917857437918cc7fd8bd3b793d30f6a346cb9
letminjae <cmjj0824@naver.com>
2022-03-01 02:02:32 +0900

    CommentWrite 뷰 완료

--- src/components/detail/CommentWrite.js
+++ src/components/detail/CommentWrite.js
@@ -1,16 +1,58 @@
-import React from "react";
-import Button from "../../elements/Button";
-import Grid from "../../elements/Grid";
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { history } from '../../redux/configStore'
 
 const CommentWrite = (props) => {
-    return (
-        <>
-            <Grid display="flex">
-                <div>댓글 작성칸</div>
-                <Button>작성</Button>
-            </Grid>
-        </>
-    )
-};
+
+  const [comment, setComment] = useState("")
+
+  const onChangeComment = (e) => {
+    setComment(e.target.value)
+  }
+
+  return (
+    <>
+      <CommentWriteContainer>
+        <ImgInput>
+          <input className='writebox'
+            placeholder="토론에 대한 댓글을 작성해주세요"
+            type="text"
+            value={comment}
+            onChange={onChangeComment}
+          ></input>
+        </ImgInput>
+        <button style={{ cursor: 'pointer' }} onClick={null}>아이콘</button>
+      </CommentWriteContainer>
+
+    </>
+  )
+}
+
+const CommentWriteContainer = styled.div`
+  bottom: 0;
+  width: 100%;
+  padding: 8px;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  align-items: center;
+  background: #fcfcfc;
+`
+const ImgInput = styled.div`
+  width: 100%;
+  display: flex;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  align-items: center;
+  .writebox {
+    border: none;
+    font-size: 16px;
+    padding: 0 20px 0 0;
+    width: calc(100% - 50px);
+    &::placeholder {
+      color: gray
+    }
+  }
+`
 
 export default CommentWrite;
