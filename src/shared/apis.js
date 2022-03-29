effc22cf06526586eecf0e6dd3ce7ae04ffa573c
jeonhaekang <73621658+jeonhaekang@users.noreply.github.com>
2022-03-29 19:09:00 +0900

    no message

--- src/shared/apis.js
+++ src/shared/apis.js
@@ -5,8 +5,8 @@ import { actionCreators as alertAction } from "../redux/modules/alert";
 import store from "../redux/configStore";
 
 export const instance = axios.create({
-  baseURL: "http://ddanddan.shop",
-  // baseURL: "https://raddas.site",
+  // baseURL: "http://ddanddan.shop",
+  baseURL: "https://raddas.site",
   // baseURL: "https://dongseok.shop",
   // baseURL: "http://ddanddan.shop/",
 });
