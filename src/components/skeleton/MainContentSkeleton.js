ba49868f6fd91f1f9e65351fc8bb64991f7dadcb
jeonhaekang <73621658+jeonhaekang@users.noreply.github.com>
2022-04-02 19:38:28 +0900

    스켈레톤 ui 수정

--- src/components/skeleton/MainContentSkeleton.js
+++ src/components/skeleton/MainContentSkeleton.js
@@ -1,33 +1,19 @@
-import styled from "styled-components";
 import FlexGrid from "../../elements/FlexGrid";
 import CategorySkeleton from "../../elements/skeleton/CategorySkeleton";
 import MainCardSkeleton from "../../elements/skeleton/MainCardSkeleton";
 
 const MainContentSkeleton = () => {
   return (
-    <FlexGrid is_column gap="82px">
-      <FlexGrid is_column>
-        <SkeletonText />
-        <SkeletonText />
-      </FlexGrid>
-      <FlexGrid is_column gap="24px">
-        <MainCardSkeleton />
-        <MainCardSkeleton />
-        <MainCardSkeleton />
-        <CategorySkeleton />
-        <MainCardSkeleton />
-        <MainCardSkeleton />
-        <MainCardSkeleton />
-      </FlexGrid>
+    <FlexGrid is_column marginTop="83px">
+      <MainCardSkeleton />
+      <MainCardSkeleton />
+      <MainCardSkeleton />
+      <CategorySkeleton />
+      <MainCardSkeleton />
+      <MainCardSkeleton />
+      <MainCardSkeleton />
     </FlexGrid>
   );
 };
-
-const SkeletonText = styled.div`
-  background-color: #e7e7e7;
-  width: 70%;
-  height: 33px;
-  border-radius: 15px;
-`;
 
 export default MainContentSkeleton;
