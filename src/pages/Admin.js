6877a9afc43a0abbf99cb4e84f99a498c617e721
jeonhaekang <73621658+jeonhaekang@users.noreply.github.com>
2022-04-05 14:22:23 +0900

    관리자 페이지 접근 불가 설정

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 import WarnUser from "../components/admin/WarnUser";
@@ -8,17 +8,31 @@ import Notice from "../components/admin/Notice";
 import GivePoint from "../components/admin/GivePoint";
 import WarnComment from "../components/admin/WarnComment";
 import Banner from "../components/admin/Banner";
+import { useSelector } from "react-redux";
+import { history } from "../redux/configStore";
 
 const Admin = () => {
+  const user = useSelector((store) => store.user.user?.role);
+  
+  const [admin, setAdmin] = useState(false);
+  
+  useEffect(() => {
+    if (user) user === "ADMIN" ? setAdmin(true) : history.goBack();
+  }, [user]);
+
   return (
     <AdminWrap>
-      <BlindBoard />
-      <BlindRoom />
-      <WarnUser />
-      <Notice />
-      <GivePoint />
-      <WarnComment />
-      <Banner />
+      {admin && (
+        <>
+          <BlindBoard />
+          <BlindRoom />
+          <WarnUser />
+          <Notice />
+          <GivePoint />
+          <WarnComment />
+          <Banner />
+        </>
+      )}
     </AdminWrap>
   );
 };
